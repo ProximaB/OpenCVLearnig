050c7a2a9d97414de825526baa7724ddc87494c7
proximab <bartek.smarzeniec@gmail.com>
2018-09-16 22:16:32 +0200

    threshold

--- tut5.py
+++ tut5.py
@@ -7,6 +7,13 @@ img1_inSizeImg2 = img1[0:172, 0:293]
 # (155,211,79) + (50,170,200) = 205, 381, 279...translated to (205,255,255)
 #add = cv2.add(img1_inSizeImg2*2, img2*2) #add = cv2.add(img1_inSizeImg2*2, img2*2)
 #weighted = cv2.addWeighted(img1_inSizeImg2, 0.6, img2, 0.4, 0)
-cv2.imshow('weighted', weighted)
+#cv2.imshow('weighted', weighted)
+
+rows, cols, channels = img2.shape
+roi = img1[0:rows, 0:cols]
+
+img2gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+ret, mask = cv2.threshold(img2gray, 220, 255, cv2.THRESH_BINARY_INV)
+cv2.imshow('mask', mask)
 cv2.waitKey(10*1000)
 cv2.destroyAllWindows()
