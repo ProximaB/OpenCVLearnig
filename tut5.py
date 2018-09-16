18624242053c99fd16d8e5d3744c5d0630b90238
proximab <bartek.smarzeniec@gmail.com>
2018-09-16 22:09:26 +0200

    tut5

--- tut5.py
+++ tut5.py
@@ -3,8 +3,10 @@ import numpy as np
 img1 = cv2.imread('merida.jpg')
 img2 = cv2.imread('image.jpeg')
 img1_inSizeImg2 = img1[0:172, 0:293]
-add = img1_inSizeImg2 + img2
-
-cv2.imshow('add', add)
-cv2.waitKey(0)
+#add = img1_inSizeImg2 + img2
+# (155,211,79) + (50,170,200) = 205, 381, 279...translated to (205,255,255)
+#add = cv2.add(img1_inSizeImg2*2, img2*2) #add = cv2.add(img1_inSizeImg2*2, img2*2)
+#weighted = cv2.addWeighted(img1_inSizeImg2, 0.6, img2, 0.4, 0)
+cv2.imshow('weighted', weighted)
+cv2.waitKey(10*1000)
 cv2.destroyAllWindows()
