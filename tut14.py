fc0d43c9689a731698c1d2b4928c20f16698f1fb
proximab <bartek.smarzeniec@gmail.com>
2018-09-23 08:55:49 +0200

    14 - Feature Matching (Homography) Brute Force

--- tut14.py
+++ tut14.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 img1 = cv2.imread('zegarek.jpeg', 0)
 
@@ -17,11 +18,12 @@ bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
 matches = bf.match(des1, des2)
 matches = sorted(matches, key = lambda x:x.distance)
 
-img2 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:12], None, flags=2)
+img2 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:9], None, flags=2)
 plt.imshow(img2)
 plt.show()
-while True
+while True:
 	k = cv2.waitKey(5) & 0xFF
-		if k == 27:
-			break;
-
+	if k == 27:
+		cv2.destroyAllWindows()
+		sys.exit(0)
+		break;
